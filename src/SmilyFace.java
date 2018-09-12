49092ea5518dfe4919a565d4055c9da30daeb54b
Daniel <42978531+danielhan1@users.noreply.github.com>
2018-09-12 14:07:13 -0400

    Add files via upload

---
+++ src/SmilyFace.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Daniel Han
+ * Smily Face
+ *
+ */
+
+public class SmilyFace 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| * * |\n|  *  |");
+		
+		
+		
+	}
+	
+	}
